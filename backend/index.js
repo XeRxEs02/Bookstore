ec0ecd4fa494ba32acc4e53c5164651fabba489f
XeRxEs02 <158118278+XeRxEs02@users.noreply.github.com>
2024-10-24 11:59:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 const app=express();
 app.use(express.json());
-app.use(cors());
-//app.use(
-  //  cors({
-    //origin:'http://localhost:5555',
-    //methods:['GET','POST','PUT','DELETE'],
-    //allowedHeaders:['Content-Type'],
-//}));
+
+app.use(
+    cors({
+    origin:'http://localhost:5555',
+    methods:['GET','POST','PUT','DELETE'],
+allowedHeaders:['Content-Type'],
+}));
 app.get('/',(request,response)=>{
     console.log(request);
     return response.status(234).send('HEY THERE WELCOME!');
